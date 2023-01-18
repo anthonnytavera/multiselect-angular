b361b88594c3a77854d7a936b9b833d061c35497
Anthonny Tavera <anthonnytavera@gmail.com>
2023-01-18 16:01:59 -0400

    implementacion

--- multiselect/src/app/app.component.ts
+++ multiselect/src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multiselect';
+  title = 'angular-multiselect';
+
+  selectedOption!: any;
+  optionsSelect = [
+    {
+      value: 2,
+      name: "APRA"
+    },
+    {
+      value: 3,
+      name: "DGHySA"
+    },
+    {
+      value: 4,
+      name: "DGFyC"
+    },
+    {
+      value: 5,
+      name: "DGFyCO"
+    },
+    {
+      value: 6,
+      name: "DGFyC"
+    },
+    {
+      value: 7,
+      name: "DGPT"
+    }
+  ];
+
+
+  onChange() {
+    console.log(this.selectedOption);
+  }
 }
+	
